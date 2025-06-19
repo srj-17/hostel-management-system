414e2e5d321b2d946d10da6d80a730619989a21a
sujan <sajantmg449@gmail.com>
2025-06-19 17:13:58 +0545

    feat: Add custom errors and error handling

--- packages/backend/app.js
+++ packages/backend/app.js
@@ -11,12 +11,24 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 const router = require("./routes")
+const NotFoundError = require("./customErrors/NotFoundError")
 
 // routes
 app.use("/students", router.student)
 app.use("/attandances", router.attandance)
 app.use("/beds", router.bed)
 app.use("/payments", router.payment)
+
+// error handling
+app.use((req, res, next) => {
+    throw new NotFoundError()
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.statusCode || 500).json({
+        msg: err.message
+    })
+})
 
 const PORT = process.env.PORT
 app.listen(process.env.PORT, () => {
