2df59e4e059749c94f2b35fba943d9e4a2669ba2
srj-17 <saugatrijal17@gmail.com>
2025-06-25 06:52:24 +0545

    feat: Write some tests

--- packages/backend/tests/students.test.js
+++ packages/backend/tests/students.test.js
@@ -1,6 +1,27 @@
 const app = require("./test_app")
 const request = require("supertest")(app)
+const db = require("../models")
+
+afterEach(async () => {
+    await db.student.deleteMany()
+})
 
 test("students route sends json", (done) => {
-    request.get("/students").expect("Content-Type", /json/).expect(200, done)
+    request
+        .post("/students")
+        .type("application/json")
+        .send({
+            firstName: "Ram",
+            lastName: "Rijal",
+            email: "saugat@rijal.com",
+            dob: "2020-10-20",
+            phone: 980000000,
+            address: "jhapa",
+        })
+        .then(() => {
+            request
+                .get("/students")
+                .expect("Content-Type", /json/)
+                .expect(200, done)
+        })
 })
